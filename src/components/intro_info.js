358a0ded58c452f6d37b289fc6e36c11c4533a60
nicogrupe <nico.grupe@gmail.com>
2019-11-02 11:39:54 -0300

    profile picture added

--- src/components/intro_info.js
+++ src/components/intro_info.js
@@ -5,7 +5,7 @@ class IntroInfo extends React.Component {
     return (
       <div className="white-text text-center pt-5">
         <h1 className="display-2 mb-4 dark-grey-text wow fadeIn">
-          I am <strong>Nicolas Grupe</strong>
+          I am <strong>Nicolás Grupe</strong>
         </h1>
         <h5
           className="text-uppercase font-weight-bold wow fadeIn"
@@ -13,8 +13,21 @@ class IntroInfo extends React.Component {
         >
           <mark>Full stack developer</mark>
         </h5>
+
+        <div className="text-center wow fadeIn" 
+          id="img-profile-div"
+          data-wow-delay="0.4s">
+          <img
+            id="img-profile"
+            src="./img/profile.jpg"
+            className="img-fluid "
+            alt=""
+          />
+        </div>
+
         <a
           href="#about"
+          id="to-about"
           className="btn btn-floating btn-large wow fadeIn"
           data-wow-delay="0.4s"
           data-offset={100}

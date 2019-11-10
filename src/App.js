5ba1021a9a87918219e7bf76c9db4a2805552d0b
nicogrupe <nico.grupe@gmail.com>
2019-11-10 16:45:06 -0300

    stable

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 // import logo from './logo.svg';
 import Root from './components/root';
-// import './App.css';
-import "./nigr.scss";
+import './App.css';
+// import "./nigr.scss";
+import './index.css';
 
 class App extends Component {
   
